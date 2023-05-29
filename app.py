74d281f716fdf7da57439822bd6a42488f6c89fd
prathamesh-coditas <98110316+prathamesh.chaudhari@coditas.com>
2023-05-29 17:02:11 +0530

    config updated

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://pratham:lU7tAtBQFZnAARK0psAhsIfNfVoIxMVI@dpg-chq827u7avjb90ludveg-a.oregon-postgres.render.com/todo_lsbr"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 db = SQLAlchemy(app)
 
